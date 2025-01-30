59b6a0be3955c5211d4594058946d24793db593e
MOHAMED <ahmadiin612@gmail.com>
2025-01-30 21:59:55 +0300

    added 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 export default function App() {
   return(
     <main>
-      <h1>hello, world</h1>
     </main>
   )
 }
---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,7 @@
+export default function BottomBar() {
+    return(
+        <nav>
+            
+        </nav>
+    )
+}
