d592126e8eefc3642b95aa4e722130dad1772310
MOHAMED <ahmadiin612@gmail.com>
2025-02-06 23:43:37 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Footer } from "./components/Footer"
+import TopBar from "./components/TopBar"
+import BottomBar from "./components/BottomBar"
+
 export default function App() {
   return(
     <main>
+      <TopBar />
+      <BottomBar />
+      <Footer />
     </main>
   )
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,8 @@
 export default function TopBar() {
     return(
         <nav>
-            
+            <a href=""><img src="" alt="" /></a>
+            <a href="locatio">Deliver To</a>
         </nav>
     )
 }
