0d39dc27ddca7a0702667ea1e67d5913b356f156
MOHAMED <ahmadiin612@gmail.com>
2025-02-08 22:50:58 +0300

    changes on tailwind css

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,7 +1,12 @@
 export default function BottomBar() {
     return(
         <nav>
-            
+            <i class="fa-solid fa-bars">All</i>
+            <a href="">Today's deals</a>
+            <a href="">Customer Services</a>
+            <a href="">Registry</a>
+            <a href="">Gift Cards</a>
+            <a href="">Sell</a>
         </nav>
     )
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,7 @@
 export default function TopBar() {
     return(
         <nav>
-            <a href=""><img src="" alt="" /></a>
+            <a href=""><img src="src/images/amazon.png" alt="" className="w-16"/></a>
             <a href="locatio">Deliver To</a>
         </nav>
     )
