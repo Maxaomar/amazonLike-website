7e2dfb3a61d0c6a33e64c33d43e422e436e40184
MOHAMED <ahmadiin612@gmail.com>
2025-02-16 21:45:46 +0300

    modified navs

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,12 +1,14 @@
 export default function BottomBar() {
     return(
-        <nav>
-            <i class="fa-solid fa-bars">All</i>
+        <header className="bg-slate-950 text-white">
+        <nav className="">
+            <i class="fa-solid fa-bars">all</i>
             <a href="">Today's deals</a>
             <a href="">Customer Services</a>
             <a href="">Registry</a>
             <a href="">Gift Cards</a>
             <a href="">Sell</a>
         </nav>
+        </header>
     )
 }
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,8 +1,14 @@
 export default function TopBar() {
     return(
-        <nav>
+        <header className="bg-slate-500 flex align-middle">
+        <nav className="flex">
             <a href=""><img src="src/images/amazon.png" alt="" className="w-16"/></a>
-            <a href="locatio">Deliver To</a>
+            <a href="">Deliver To</a>
+            <a href="locatio"><img src="src/images/marker.png" alt="" /></a>
+
+            <input type="text"  className="w-96 h-6"/>
+
         </nav>
+        </header>
     )
 }
