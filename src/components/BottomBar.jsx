ba1cc32b474b370fe8b7466317bbbd5e2983abdb
MOHAMED <ahmadiin612@gmail.com>
2025-04-02 21:36:41 +0300

    updated bottombar

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -3,7 +3,7 @@ export default function BottomBar() {
         <header className="bg-slate-950 text-white flex items-center">
              <a href=""> <i class="fa-solid fa-bars"></i> All</a>
 
-        <nav className="">
+        <nav className="flex gap-4 ml-3">
                 <a href="">Today's deals</a>
                 <a href="">Customer Services</a>
                 <a href="">Registry</a>
