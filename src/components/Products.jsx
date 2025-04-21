e245bfd5bea1df73754bd6ca2372b83732ec32b8
MOHAMED <ahmadiin612@gmail.com>
2025-04-21 21:22:17 +0300

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,9 +5,13 @@ export function ProductLayout() {
   })
 
   return(
-    <main className="flex">
+    <main className="products">
         <article className="">
-            <img src="src/assets/images/products/athletic-cotton-socks-6-pairs.jpg" alt="2-pair of trouser" />
+          <div className="product-image">
+              <img src="src/assets/images/products/athletic-cotton-socks-6-pairs.jpg" alt="2-pair of trouser" />
+          </div>
+
+
             <h2>Black and Gray Athletic Cotton Socks - 6 Pairs</h2>
 
               <div className="flex gap-5 items-center">
@@ -17,7 +21,22 @@ export function ProductLayout() {
 
               <div>$10.90</div>
 
-              <button>Add to cart</button>
+              <div>
+              <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+              </select>
+              </div>
+
+              <button className="add-button">Add to cart</button>
          </article>
     </main>
   )
