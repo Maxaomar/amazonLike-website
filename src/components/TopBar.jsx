dcbce1ec8f6b6572f1040e3b8ff82504e5430ea0
MOHAMED <ahmadiin612@gmail.com>
2025-05-20 21:42:02 +0300

    updated top navbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -5,9 +5,9 @@ import  '../styles/topbar.css'
 
 export function TopBar() {
     return(
-        <nav className='nav-container flex items-center'>
+        <nav className='nav-container'>
             <header className='flex'>
-                <a href=""><img src={amazonLogo} alt="amazon logo photo" className='w-16' /></a>
+                <a href=""><img src={amazonLogo} alt="amazon logo photo" className='w-24' /></a>
             </header>
 
             <div className='flex'>
@@ -16,18 +16,19 @@ export function TopBar() {
             </div>
 
             <div className="flex">
-                <select className='bg-amber-600 w-12 h-8'>
+                <select className='w-16 bg-selections'>
                     <option>All</option>
                     <option>Electronics</option>
                     <option>Beauty Tools & Accessories</option>
                     <option>Books</option>
                 </select>
+
+                <div className='search-bar bg-yellow-300'>
+                    <input type="text" placeholder="Search Amazon" />
+                    <i className="fa-solid fa-magnifying-glass" ></i>
+                </div>
             </div>
 
-            <div className='search-bar bg-yellow-300'>
-                <input type="text" placeholder="Search Amazon" />
-                <i className="fa-solid fa-magnifying-glass" ></i>
-            </div>
 
             <div className='flex'>
                 <select className='bg-red-400 w-12 h-8'>
