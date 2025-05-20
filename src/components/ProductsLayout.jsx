eb66eca39d8bcf908d5c51d8e26d8de400249d12
MOHAMED <ahmadiin612@gmail.com>
2025-05-20 21:12:05 +0300

    removed some css

--- src/components/ProductsLayout.jsx
+++ src/components/ProductsLayout.jsx
@@ -1,5 +1,3 @@
-import '../styles/products.css'
-
 export function ProductsLayout(props) {
   return(
       <main className="product-container">
@@ -8,16 +6,16 @@ export function ProductsLayout(props) {
                   <img src="src/assets/images/products/athletic-cotton-socks-6-pairs.jpg" alt="2-pair of trouser" />
               </div>
 
-              <h2>{props.name}</h2>
+              <h2 className=''>{props.name}</h2>
 
           <div className="flex gap-5 items-center">
               <div><img src="src/assets/images/ratings/rating-05.png" alt="" /></div>
               <span>87</span>
           </div>
 
-          <div>{props.priceCents}</div>
+          <div className=''>{props.priceCents}</div>
 
-          <div>
+          <div className=''>
               <select>
                  <option value="1">1</option>
                  <option value="2">2</option>
@@ -32,8 +30,11 @@ export function ProductsLayout(props) {
               </select>
           </div>
 
-          <button className="add-button">Add to cart</button>
-</article>
+          <div className='flex'>
+               <button className="add-button">Add to cart</button>
+          </div>
+
+   </article>
 </main>
   )
 }
