36bf1fc73a5b8088207734b2c24c39f603e79781
MOHAMED <ahmadiin612@gmail.com>
2025-05-20 06:44:26 +0300

    updates

--- src/components/ProductsLayout.jsx
+++ src/components/ProductsLayout.jsx
@@ -17,22 +17,22 @@ export function ProductsLayout(props) {
 
           <div>{props.priceCents}</div>
 
-<div>
-<select>
-<option value="1">1</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">10</option>
-</select>
-</div>
+          <div>
+              <select>
+                 <option value="1">1</option>
+                 <option value="2">2</option>
+                 <option value="3">3</option>
+                 <option value="4">4</option>
+                 <option value="5">5</option>
+                 <option value="6">6</option>
+                 <option value="7">7</option>
+                 <option value="8">8</option>
+                 <option value="9">9</option>
+                 <option value="10">10</option>
+              </select>
+          </div>
 
-<button className="add-button">Add to cart</button>
+          <button className="add-button">Add to cart</button>
 </article>
 </main>
   )
