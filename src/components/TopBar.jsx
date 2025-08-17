9d297460251baee6a2bb5895884cadd75eaafc09
MOHAMED <ahmadiin612@gmail.com>
2025-08-17 14:45:43 +0300

    changes in navbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -6,23 +6,23 @@ import  '../styles/topbar.css'
 export function TopBar() {
     return(
         <nav className='nav-container'>
-            <header className='flex'>
-                <a href=""><img src={amazonLogo} alt="amazon logo photo" className='w-24' /></a>
-            </header>
+                <div className='flex'>
+                    <a href=""><img src={amazonLogo} alt="amazon logo photo" className='w-24' /></a>
+                </div>
 
-            <div className='flex'>
-                <a href="" className='text-xs'>Deliver To
+                <div className='flex'>
+                    <a href="" className='text-xs'>Deliver To</a>
                     <img src="src/images/marker.png" alt="" className='max-w-4'/>
-                </a>
-            </div>
+                    <h3>kenya</h3>
+                </div>
 
-            <div className="flex">
-                <select className='w-12 bg-selections'>
-                    <option>All</option>
-                    <option>Electronics</option>
-                    <option>Beauty Tools & Accessories</option>
-                    <option>Books</option>
-                </select>
+                <div className="flex">
+                    <select className='w-12 bg-selections'>
+                        <option>All</option>
+                        <option>Electronics</option>
+                        <option>Beauty Tools & Accessories</option>
+                        <option>Books</option>
+                    </select>
 
                 <div className='search-bar bg-yellow-300'>
                     <input type="text" placeholder="Search Amazon" className=''/>
@@ -31,28 +31,24 @@ export function TopBar() {
             </div>
 
 
-            <div className='flex'>
-                <select className='bg-red-400 w-8 h-8'>
-                    <option value="English">EN</option>
-                    <option value="">AR</option>
-                    <option value="">SM</option>
-                    <option value="">KI</option>
-                    <option value="">ES</option>
-                </select>
-            </div>
+                    <select className='bg-red-400 w-8 h-8'>
+                        <option value="English">EN</option>
+                        <option value="">AR</option>
+                        <option value="">SM</option>
+                        <option value="">KI</option>
+                        <option value="">ES</option>
+                    </select>
 
-            <div className='flex'>
-                <select className='bg-amber-600 w-12 h-8'>
-                    <option value="">Hello, sign in</option>
-                    <h2>Accounts & Lists</h2>
-                </select>
-            </div>
+                    <select className='bg-amber-600 w-12 h-8'>
+                        <option value="">Hello, sign in</option>
+                        <h2>Accounts & Lists</h2>
+                    </select>
 
 
-            <div className='flex'>
-                <h3>Returns & Orders</h3>
-                <img src={cartIcon} alt="" className='w-12'/>
-            </div>
+                    <div className='flex'>
+                        <h3>Returns & Orders</h3>
+                        <img src={cartIcon} alt="" className='w-12'/>
+                    </div>
     </nav>
 )
 }
